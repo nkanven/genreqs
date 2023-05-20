0fc2dad763f5acaeec5e46c7636b5bf5b68b4f6c
Nkondog A. Venceslas <nkanven@gmail.com>
2023-05-20 21:26:36 +0100

    Done terminal display and master requirements search

--- genreqs/__main__.py
+++ genreqs/__main__.py
@@ -1,5 +1,5 @@
 # __main__.py
-import sys, os, getopt
+import sys, os, getopt, logging
 
 sys.path.append(os.path.abspath(__file__ + "../../../"))
 
@@ -10,9 +10,9 @@ def main():
     """
         --- Generate requirements for you project ---
     """
-
     argv = sys.argv[1:]
     pip_list = []
+    path = ""
 
     try:
         options, args = getopt.getopt(
@@ -22,10 +22,10 @@ def main():
 
         for name, value in options:
             if name.strip()  == '-p' or name.strip() == '--path':
-                print(name, value)
+                path = value
                 pip_list = parse.minimal_task(value)
     except Exception as e:
-        print("Something wrong happened", str(e))
+        logging.warning(f"Something wrong happened {str(e)}")
 
     if len(sys.argv) == 1:
         #print("Need help ? Add -h or --help to your command")
@@ -43,10 +43,22 @@ Usage:
         -p, --path              Generate requirements.txt from python folder / virtual environment
 """
         if '-V' in sys.argv or '--version' in sys.argv:
-            print(f"GenReqs version {genreqs.__version__}")
+            logging.info(f"GenReqs version {genreqs.__version__}")
     
     if pip_list:
-        parse.get_only_master_packages(pip_list)
+        master_packages = parse.get_only_master_packages(pip_list, path)
+        parse.create_requirements_txt(master_packages)
+        working_dir = os.getcwd()
+        separator = "\\" if os.getcwd().find("\\") != -1 else "/"
+        msg = '\33[32m' + "\nTask completed! "+'\33[0m' + "\nPlease find your requirements.txt at "
+        req_path = working_dir + separator + "requirements.txt"
+
+        clikable = f"\x1b]8;;{req_path}\a{req_path}\x1b]8;;\a"
+        
+        sys.stdout.write(msg + clikable)
+        sys.stdout.flush()
+    else:
+        logging.info("No python package has been installed on this environment")
 
 if __name__ == "__main__":
     main()
