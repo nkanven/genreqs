110336679145db79bc8ac863cf7e122de57b0f70
Nkondog A. Venceslas <nkanven@gmail.com>
2023-05-20 21:28:11 +0100

    Dev status set at Beta

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     keywords = ['Requirements.txt', 'Generate', 'requirements', 'packages', 'dependancy'],   # Keywords that define your package best
     install_requires=[],           # I get to this in a second,
     classifiers=[
-      'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+      'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
       'Intended Audience :: Developers',      # Define that your audience are developers
       'Topic :: Software Development :: Build Tools',
       'License :: OSI Approved :: MIT License',   # Again, pick a license
